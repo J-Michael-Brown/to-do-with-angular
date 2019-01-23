b70f81b128ee2b0fd678beab9431050bcd9474d3
Christopher Cahill and Michael Brown <christopher@>
2019-01-23 15:35:05 -0800

    add input and output to task list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 import { Task } from './models/task.model'
 
 @Component({
@@ -18,23 +19,29 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
 
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course', 3),
-    new Task('Begin brainstorming possible JavaScript group projects', 2),
-    new Task('Add README file to last few Angular repos on GitHub', 2)
-  ];
+  selectedTask: Task = null;
 
- editTask() {
-    console.log('edit!')
+  masterTaskList: Task[] = [
+      new Task('Finish weekend Angular homework for Epicodus course', 3),
+      new Task('Begin brainstorming possible JavaScript group projects', 2),
+      new Task('Add README file to last few Angular repos on GitHub', 2)
+
+    ];
+
+  anEvenBiggerListOfTasks: Task[] = [
+        new Task('Finish weekend Angular homework for Epicodus course', 3),
+        new Task('Begin brainstorming possible JavaScript group projects', 2),
+        new Task('Add README file to last few Angular repos on GitHub', 2),
+        new Task('another thing to do',1)
+  ]
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
   }
 
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
+  finishedEditing() {
+    this.selectedTask = null;
   }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditTaskComponentComponent } from './edit-task-component/edit-task-component.component';
+import { NewTaskComponentComponent } from './new-task-component/new-task-component.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditTaskComponentComponent,
+    NewTaskComponentComponent,
+    TaskListComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
